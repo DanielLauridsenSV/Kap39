7e5de343653d88249d82da862a43ec66f7a00381
Daniel Lauridsen <daniel@pingas.se>
2021-10-31 14:59:48 +0100

    Add project files.

---
+++ Files.IO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Collections;
+
+namespace Files.IO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int userinput =0;
+            Console.WriteLine("write your name");
+            string username = Console.ReadLine();
+            if (File.Exists($"{username}.txt"))
+            {
+                userinput = int.Parse(File.ReadAllText($"{username}.txt"));
+            }
+
+            bool continueplay = true;
+            
+            while (continueplay)
+            {
+                Console.WriteLine($" current score is :{userinput}");
+                ConsoleKey consoleKey = Console.ReadKey().Key;
+                if (consoleKey == ConsoleKey.Escape)
+                {
+                    continueplay = false;
+                    break;
+                }
+
+                userinput ++;
+
+                Console.Clear();
+
+            }
+            string score = ($"{userinput}\n");
+            File.WriteAllText($"{username}.txt",score);
+        }
+
+    }
+}
---
+++ Files.IO/Score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files.IO
+{
+    internal class Score
+    {
+        public int Scoreint { get; set; }
+        public string Name { get; set; }
+
+        public Score (int score,string name)
+        {
+            Scoreint = score;
+            Name = name;
+        }
+
+    }
+
+}
